fd7add4d53aead3975fc32fba5364fc2bef25183
Jean Delince ROMELUS <rjeandelince@gmail.com>
2022-02-14 15:32:34 +0100

    Mon premier projet reactJS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,27 @@
 import React from "react"
 import ReactDOM from "react-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./functionBased/components/TodoContainer"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+import About from "./functionBased/pages/About"
+import NotMatch from "./functionBased/pages/NotMatch"
+import Navbar from "./functionBased/components/Navbar"
+import SinglePage from "./functionBased/pages/SinglePage"
 
 //stylesheet
-import "./App.css"
+import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <TodoContainer />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TodoContainer/>} />
+        <Route path="/about" element={<About />} />
+        <Route  path="/about/:slug" element={ <SinglePage /> }/>
+        <Route path="*" element={<NotMatch/>} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
