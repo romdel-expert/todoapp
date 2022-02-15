0edd787e9b8de69f58985c46598c037d7e074b2c
Jean Delince ROMELUS <rjeandelince@gmail.com>
2022-02-15 10:48:23 +0100

    Ajout page 404.html dans public et modification de index.html

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 //component file
 import TodoContainer from "./functionBased/components/TodoContainer"
-// import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
-import { HashRouter as Router, Route, Routes} from "react-router-dom"
+import { BrowserRouter as Router, Route, Routes} from "react-router-dom"
+// import { HashRouter as Router, Route, Routes} from "react-router-dom"
 import About from "./functionBased/pages/About"
 import NotMatch from "./functionBased/pages/NotMatch"
 import Navbar from "./functionBased/components/Navbar"
@@ -14,7 +14,7 @@ import "./functionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<TodoContainer/>} />
